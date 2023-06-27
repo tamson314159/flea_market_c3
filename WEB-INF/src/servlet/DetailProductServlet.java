d40997e63c9d0b0be66c5220ecbcfae5a327f07a
tamson314159 <102057388+tamson314159@users.noreply.github.com>
2023-06-27 17:54:12 +0900

    Add files via upload

--- WEB-INF/src/servlet/DetailProductServlet.java
+++ WEB-INF/src/servlet/DetailProductServlet.java
@@ -15,7 +15,8 @@ public class DetailProductServlet extends HttpServlet {
 		//変数宣言
 		String error = "";
 		String cmd = "";
-		String product_id = "";
+		String strproduct_id = "";
+		int product_id = 0;
 
 		try {
 			//ProductDAOクラスのオブジェクトを生成します。
@@ -28,18 +29,20 @@ public class DetailProductServlet extends HttpServlet {
 			request.setCharacterEncoding("UTF-8");
 
 			//画面から送信されるproduct_idとcmd情報を受け取ります。
-			product_id = request.getParameter("product_id");
+			strproduct_id = request.getParameter("product_id");
+			cmd = request.getParameter("cmd");
 
-
-			//SelectByProduct_idメソッドの呼び出し
+			//商品一覧画面のProduct_idリンクをクリック
+			product_id = Integer.parseInt(strproduct_id);
 			Product = objDao.selectByProduct_id(product_id);
 
-			//取得した商品情報を「Product」という名前でリクエストスコープに登録します。
+
+			//取得した書籍情報を「Product」という名前でリクエストスコープに登録します。
 			request.setAttribute("Product", Product);
 
 			//書籍一覧画面のISBNリンクをクリック時、DBに接続できない
 			}catch(IllegalStateException e) {
-				error ="DB接続エラーの為、商品詳細は表示できませんでした。";
+				error ="DB接続エラーの為、書籍詳細は表示できませんでした。";
 				cmd = "logout";
 
 			}catch(Exception e) {
@@ -48,8 +51,7 @@ public class DetailProductServlet extends HttpServlet {
 
 			}finally {
 				if(error.equals("")) {
-
-						request.getRequestDispatcher("/view/detailProduct.jsp").forward(request, response);
+				request.getRequestDispatcher("/view/detailProduct.jsp").forward(request, response);
 
 				}else {
 					request.setAttribute("error",error);
