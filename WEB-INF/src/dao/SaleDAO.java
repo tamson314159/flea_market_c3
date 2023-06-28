51afa5c0958f551468612e8a0c753329de3821e9
tamson314159 <102057388+tamson314159@users.noreply.github.com>
2023-06-28 14:35:21 +0900

    Add files via upload

--- WEB-INF/src/dao/SaleDAO.java
+++ WEB-INF/src/dao/SaleDAO.java
@@ -5,13 +5,9 @@ import java.util.*;
 import bean.*;
 
 public class SaleDAO {
-
 	private static String RDB_DRIVE = "com.mysql.jdbc.Driver";
-
 	private static String URL = "jdbc:mysql://localhost/flea_market_db";
-
 	private static String USER = "root";
-
 	private static String PASSWD = "root123";
 
 	private static Connection getConnection() {
@@ -26,9 +22,8 @@ public class SaleDAO {
 	}
 
 	// 管理者権限
-	//出品商品の詳細を取得するメソッド(No,10 DetailListingAdminServlet)
-	public ArrayList<Sale> selectAllAdmin() {
-
+	// 出品商品の詳細を取得するメソッド(No,10 DetailListingAdminServlet)
+	public ArrayList<Sale> selectAllAdmin(String product_id) {
 		Connection con = null;
 		Statement smt = null;
 
@@ -38,13 +33,10 @@ public class SaleDAO {
 		// SQL文
 		String sql = "SELECT a.product_name,a.kinds,a.price,a.quantity,a.remarks,a.region,"
 				+ "a.exhibition_date,a.update_date,a.image,a.transaction,a.user_id,"
-				+ "b.purchase_user_id FROM product a,purchase_info b WHERE a.product_id = b.product_number";
-
+				+ "b.purchase_user_id FROM product a,purchase_info b WHERE " + product_id;
 		try {
-
 			con = getConnection();
 			smt = con.createStatement();
-
 			// SQL文をDBへ発行
 			ResultSet rs = smt.executeQuery(sql);
 
@@ -64,10 +56,8 @@ public class SaleDAO {
 				sale.setTransaction(rs.getString("transaction"));
 				sale.setExhibition_userid(rs.getString("exhibition_userid"));
 				sale.setPurchase_userid(rs.getString("purchase_userid"));
-
 				saleList.add(sale);
 			}
-
 		} catch (Exception e) {
 			throw new IllegalStateException(e);
 		} finally {
@@ -89,8 +79,7 @@ public class SaleDAO {
 
 	// ユーザー側
 	// 出品商品の詳細を取得するメソッド(No,23 DetailListingServlet)
-	public ArrayList<Sale> selectAllUser() {
-
+	public ArrayList<Sale> selectAllUser(String product_id) {
 		Connection con = null;
 		Statement smt = null;
 
@@ -100,16 +89,14 @@ public class SaleDAO {
 		// SQL文
 		String sql = "SELECT a.product_name,a.kinds,a.price,a.quantity,a.remarks,a.region,"
 				+ "a.exhibition_date,a.update_date,a.image,a.transaction,a.user_id,"
-				+ "b.purchase_user_id,b.purchase_date,b.money_received,b.delivery FROM product a,purchase_info b WHERE a.product_id = b.product_number";
-
+				+ "b.purchase_user_id,b.purchase_date,b.money_received,b.delivery FROM product a,purchase_info b WHERE"
+				+ product_id;
 		try {
-
 			con = getConnection();
 			smt = con.createStatement();
 
 			// SQL文をDBへ発行
 			ResultSet rs = smt.executeQuery(sql);
-
 			// 検索結果を配列に格納
 			while (rs.next()) {
 				Sale sale = new Sale();
@@ -152,7 +139,7 @@ public class SaleDAO {
 
 	// 購入ユーザー側
 	// 購入した商品の詳細を取得するメソッド(No,28 DetailOrderServlet)
-	public ArrayList<Sale> selectAllbought() {
+	public ArrayList<Sale> selectAllbought(String product_id) {
 
 		Connection con = null;
 		Statement smt = null;
@@ -163,13 +150,11 @@ public class SaleDAO {
 		// SQL文
 		String sql = "SELECT a.product_name,a.kinds,a.price,a.quantity,a.remarks,a.region,"
 				+ "a.exhibition_date,a.update_date,a.image,a.transaction,a.user_id,"
-				+ "b.purchase_date,b.money_received,b.delivery FROM product a,purchase_info b WHERE a.product_id = b.product_number";
+				+ "b.purchase_date,b.money_received,b.delivery FROM product a,purchase_info b WHERE " + product_id;
 
 		try {
-
 			con = getConnection();
 			smt = con.createStatement();
-
 			// SQL文をDBへ発行
 			ResultSet rs = smt.executeQuery(sql);
 
@@ -189,10 +174,8 @@ public class SaleDAO {
 				sale.setPurchase_date(rs.getString("purchase_date"));
 				sale.setMoney_received(rs.getString("money_received"));
 				sale.setDelivery(rs.getString("delivery"));
-
 				saleList.add(sale);
 			}
-
 		} catch (Exception e) {
 			throw new IllegalStateException(e);
 		} finally {
@@ -213,90 +196,75 @@ public class SaleDAO {
 	}
 
 	// 購入ユーザー側
-	// purchase_infoテーブルから指定ユーザーの条件に合致する購入履歴情報を取得するメソッド(No,26 ShowHistoryOrderedItemServlet)
+	// purchase_infoテーブルから指定ユーザーの条件に合致する購入履歴情報を取得するメソッド(No,26
+	// ShowHistoryOrderedItemServlet)
 	public ArrayList<Sale> selectByUser(String userid) {
+        Connection con = null;
+        Statement smt = null;
 
-		Connection con = null;
-		Statement smt = null;
+        // 検索した情報を格納するAllayListオブジェクトを生成
+        ArrayList<Sale> saleList = new ArrayList<Sale>();
+        // SQL文
+        String sql = "SELECT a.purchase_user_id , b.product_name , b.price, b.quantity,a.purchase_date,a.money_received"
+                +" FROM purchase_info a, product b"
+                + " WHERE a.product_number = b.product_id and a.purchase_user_id = '" + userid + "'";
+        try {
+            con = getConnection();
+            smt = con.createStatement();
+​
+            // SQL文をDBへ発行
+            ResultSet rs = smt.executeQuery(sql);
 
-		// 検索した情報を格納するAllayListオブジェクトを生成
-		ArrayList<Sale> saleList = new ArrayList<Sale>();
-
-		// SQL文
-		String sql = "SELECT a.purchase_user_id , b.product_name , b.price, b.quantity,a.purchase_date,a.money_received"
-				+" FROM purchase_info a, product b"
-				+ " WHERE a.product_number = b.product_id and a.purchase_user_id = '" + userid + "'";
-
-		try {
-
-			con = getConnection();
-			smt = con.createStatement();
-
-			// SQL文をDBへ発行
-			ResultSet rs = smt.executeQuery(sql);
-
-			// 検索結果を配列に格納
-			while (rs.next()) {
-				Sale sale = new Sale();
-				sale.setProduct_number(rs.getInt("product_number"));
-				sale.setProduct_name(rs.getString("product_name"));
-				sale.setPrice(rs.getInt("price"));
-				sale.setQuantity(rs.getInt("quantity"));
-				sale.setPurchase_date(rs.getString("purchase_date"));
-				sale.setMoney_received(rs.getString("money_received"));
-				saleList.add(sale);
-
-			}
-
-		} catch (Exception e) {
-			throw new IllegalStateException(e);
-		} finally {
-			if (smt != null) {
-				try {
-					smt.close();
-				} catch (SQLException ignore) {
-				}
-			}
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException ignore) {
-				}
-			}
-		}
-		return saleList;
-	}
+            // 検索結果を配列に格納
+            while (rs.next()) {
+                Sale sale = new Sale();
+                sale.setProduct_number(rs.getInt("product_number"));
+                sale.setProduct_name(rs.getString("product_name"));
+                sale.setPrice(rs.getInt("price"));
+                sale.setQuantity(rs.getInt("quantity"));
+                sale.setPurchase_date(rs.getString("purchase_date"));
+                sale.setMoney_received(rs.getString("money_received"));
+                saleList.add(sale);
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            if (smt != null) {
+                try {
+                    smt.close();
+                } catch (SQLException ignore) {
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ignore) {
+                }
+            }
+        }
+        return saleList;
+    }
 
 	// 出品ユーザー側
 	// 出品した商品の情報を更新するメソッド(No,25 UpdateProductServlet)
 	public int update(Sale sale) {
-
 		// SQL文設定
 		String sql = "UPDATE product AS a INNER JOIN purchase AS b ON a.product_id = b.product_number"
-				+ " SET a.product_name='" + sale.getProduct_name()
-				+ "',a.kinds='" + sale.getKinds()
-				+ "',a.quantity='"+ sale.getQuantity()
-				+ "',a.price='" + sale.getPrice()
-				+ "',a.remarks='" + sale.getRemarks()
-				+ "',a.region='" + sale.getRegion()
-				+ "',a.image='" + sale.getImage()
-				+ "',a.transaction ='"+ sale.getTransaction()
-				+ "',a.delivery='" + sale.getDelivery()
-				+ "' WHERE a.product_number='"+ sale.getProduct_number() + "'";
-
+				+ " SET a.product_name='" + sale.getProduct_name() + "',a.kinds='" + sale.getKinds() + "',a.quantity='"
+				+ sale.getQuantity() + "',a.price='" + sale.getPrice() + "',a.remarks='" + sale.getRemarks()
+				+ "',a.region='" + sale.getRegion() + "',a.image='" + sale.getImage() + "',a.transaction ='"
+				+ sale.getTransaction() + "',a.delivery='" + sale.getDelivery() + "' WHERE a.product_number='"
+				+ sale.getProduct_number() + "'";
 		// 変数宣言
 		Connection con = null;
 		Statement smt = null;
 		int count = 0;
-
 		try {
 			// DBに接続
 			con = getConnection();
 			smt = con.createStatement();
-
 			// SQL文発行
 			count = smt.executeUpdate(sql);
-
 		} catch (SQLException e) {
 			throw new IllegalStateException(e);
 		} finally {
@@ -314,33 +282,25 @@ public class SaleDAO {
 				}
 			}
 		}
-
 		return count;
 	}
 
 	// 管理者権限
 	// 売り上げ状況を取得するメソッド(No,11 SaleServlet)
 	public ArrayList<Sale> selectBySales() {
-
 		Connection con = null;
 		Statement smt = null;
-
 		// 購入した書籍情報を格納するAllayListオブジェクトを生成
 		ArrayList<Sale> saleList = new ArrayList<Sale>();
-
 		try {
-
 			con = getConnection();
 			smt = con.createStatement();
-
 			// SQL文
 			String sql = "SELECT b.product_name, b.price, b.region,a.purchase_date, sum(quantity) as quantity "
 					+ "FROM purchase_info a inner join product b ON a.product_id = b.product_number "
 					+ " GROUP BY  b.product_number ORDER BY  b.product_number";
-
 			// SQL文をDBへ発行
 			ResultSet rs = smt.executeQuery(sql);
-
 			// 購入情報を配列に格納
 			while (rs.next()) {
 				Sale sale = new Sale();
@@ -352,7 +312,6 @@ public class SaleDAO {
 				sale.setPurchase_date(rs.getString("purchase_date"));
 				saleList.add(sale);
 			}
-
 		} catch (Exception e) {
 			throw new IllegalStateException(e);
 		} finally {
@@ -371,5 +330,4 @@ public class SaleDAO {
 		}
 		return saleList;
 	}
-
 }
